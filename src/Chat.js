9ffe4038cf4cfc498d1fb574edfc22fd16930432
guyreuveni33 <guyhaim55@gmail.com>
2023-05-07 21:43:30 +0300

    dividing to components

--- src/Chat.js
+++ src/Chat.js
@@ -1,23 +1,21 @@
 import './Chat.css';
 import $ from "jquery";
+import ContactProfile from "./chat/contactProfile/ContactProfile";
+import HeaderProfiles from "./chat/headerProfiles/HeaderProfiles";
+import ReceivedMessage from "./chat/receivedMessage/ReceivedMessage";
+import UserMessage from "./chat/userMessage/UserMessage";
 
 function Chat() {
     return (
         <div className="mainBG d-flex justify-content-center p-4">
             <div className="chat-container">
-
                 <div className="sidebar overflow-hidden bg-dark">
                     {/* <!-- Button trigger modal -->*/}
                     <div className="userSpace overflow-hidden">
-                        <div className="ms-1">
-                            <img className="profileCurrentChatPic"
-                                 src=" https://i.postimg.cc/kGZZvf65/52-SNp-DJJCg-BVf-Ztib-Lu-XN1-Zrli-Cdvv.jpg"
-                                 alt="avatar"
-                                 width="50"></img>
-                        </div>
-                        <div className="text-white ms-2">
-                            <p className="fw-bold mb-0">Elon Musk</p>
-                        </div>
+                        <HeaderProfiles margins="ms-1" pictureSetting="profileCurrentChatPic"
+                                        profilePicture="https://i.postimg.cc/kGZZvf65/52-SNp-DJJCg-BVf-Ztib-Lu-XN1-Zrli-Cdvv.jpg"
+                                        textPosition="text-white ms-2" textSetting="fw-bold mb-0"
+                                        name="Elon Musk"></HeaderProfiles>
                         <div className="ms-auto">
                             {/*<!-- Button trigger modal -->*/}
                             <button type="button" className="btn bg-dark text-white" data-bs-toggle="modal"
@@ -32,83 +30,38 @@ function Chat() {
                             </button>
                         </div>
                     </div>
-
-
                     <ul className="list-unstyled text-white">
-                        <li className=" p-0.5 mt-2 chat-borders">
-                            <div className="d-flex justify-content-between">
-                                <div className="d-flex flex-row">
-                                    <div>
-                                        <img className="profilePic d-flex align-self-center me-3"
-                                             src="https://i.postimg.cc/BvrXRGr5/IMG-3411dvdv.jpg"
-                                             alt="avatar"  width="50"></img>
-                                    </div>
-                                    <div>
-                                        <p className="fw-bold mb-0 ms-1 me-1">Niv Swisa</p>
-                                        <p className="small ms-1">Sure!</p>
-                                    </div>
-                                </div>
-                                <p className="small mb-1 ">09/02/2023</p>
-                            </div>
-                        </li>
-                        <li className="p-0.5 mt-2 chat-borders ">
-                            <div className="d-flex justify-content-between ">
-                                <div className="d-flex flex-row">
-                                    <div>
-                                        <img className="profilePic" src="https://i.postimg.cc/L47zRrjW/dvd.jpg"
-                                             alt="avatar" className="d-flex align-self-center me-3" width="50"></img>
-                                    </div>
-                                    <div>
-                                        <p className="fw-bold mb-0 ms-1 me-1">Guy Reuveni</p>
-                                        <p className="small ms-1">Hello World!</p>
-                                    </div>
-                                </div>
-                                <p className="small mb-1">09/02/2023</p>
-                            </div>
-                        </li>
+                        <ContactProfile name="Niv Swisa" lastMessage="Sure!" date="09/02/2023"
+                                        profilePicture="https://i.postimg.cc/BvrXRGr5/IMG-3411dvdv.jpg"></ContactProfile>
+                        <ContactProfile name="Guy Reuveni" lastMessage="Hello World!" date="09/02/2023"
+                                        profilePicture="https://i.postimg.cc/L47zRrjW/dvd.jpg"></ContactProfile>
                     </ul>
                 </div>
-
                 <div className="main-chat overflow-hidden">
                     <div className="chat-header overflow-hidden">
                         {/* <!-- Contact name or chat title will be displayed here -->*/}
-
-
-                        <div>
-                            <img className="profileCurrentChatPic" src="https://i.postimg.cc/BvrXRGr5/IMG-3411dvdv.jpg"
-                                 alt="avatar" className="d-flex align-self-center me-3" width="50"></img>
-                        </div>
-                        <div className="col text-white align-self-center ">
-                            <p className="fw-bold mb-0 ms-1 ">Niv Swisa</p>
-                        </div>
+                        <HeaderProfiles pictureSetting="profileCurrentChatPic d-flex align-self-center me-3"
+                                        profilePicture="https://i.postimg.cc/BvrXRGr5/IMG-3411dvdv.jpg"
+                                        textPosition="col text-white align-self-center"
+                                        textSetting="fw-bold mb-0 ms-1 " name="Niv Swisa"></HeaderProfiles>
                         <div className="chat-input">
                             <button className="bg-dark">Log Out</button>
                         </div>
                     </div>
                     <div className="chat-messages">
                         <div>
-                            <div className="message ">
-                                <p>Hello, how are u?</p>
-                            </div>
-                            <div className="clearfix">
-                                <div className="message userTextMessage">
-                                    <p>I'm doing good, and you?</p>
-                                </div>
-                            </div>
-                            <div className="message  ">
-                                <p>Great, just about to finish my AP 2 program</p>
-                            </div>
-                            <div className="clearfix">
-                                <div className="message userTextMessage">
-                                    <p>That sounds phenomenal as Noa Kirel would say ;)</p>
-                                </div>
-                            </div>
-                            <div className="message">
-                                <p>See you next month in Italy?</p>
-                            </div>
-                            <div className="message userTextMessage">
-                                <p>Sure!</p>
-                            </div>
+                            <ReceivedMessage text="Hello, how are u?">
+                            </ReceivedMessage>
+                            <UserMessage text="I'm doing good, and you?">
+                            </UserMessage>
+                            <ReceivedMessage text="Great, just about to finish my AP 2 program">
+                            </ReceivedMessage>
+                            <UserMessage text="That sounds phenomenal as Noa Kirel would say ;)">
+                            </UserMessage>
+                            <ReceivedMessage text="See you next month in Italy?">
+                            </ReceivedMessage>
+                            <UserMessage text="Sure!">
+                            </UserMessage>
                         </div>
                     </div>
                     <div className="chat-input ">
@@ -141,4 +94,5 @@ function Chat() {
         </div>
     );
 }
+
 export default Chat;
