06c128e24612856b9a96816c12eeacac8967f012
nivswisa11 <swisaniv@gmail.com>
2023-05-11 21:31:53 +0300

    in this commit we handled the entire flow between login and register, including the validation process and a database of users.

--- src/index.js
+++ src/index.js
@@ -4,10 +4,15 @@ import './index.css';
 import Login from './Login';
 import Register from './Register';
 import Chat from './Chat';
+import {BrowserRouter, Route, Routes, Switch} from "react-router-dom";
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-
-  <React.StrictMode>
-   <Chat/>
-  </React.StrictMode>
+    <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<Login/>} />
+            <Route path="/Register" element={<Register/>} />
+            <Route path="/Chat" element={<Chat/>} />
+        </Routes>
+    </BrowserRouter>
 );
